03fcc0d71594014ddb6fd085c7c9c67b4e72aa04
yuxzho <1701210384@pku.edu.cn>
2021-08-11 18:47:48 +0800

    10/08/21

--- 200.岛屿数量.py
+++ 200.岛屿数量.py
@@ -4,8 +4,23 @@ class Node:
 
 class Solution:
     def numIslands(self, grid) -> int:
+
+        def infect(i, j, src, dst, flag):
+            if i < 1 or j < 1 or i >=len(flag) or j >= len(flag[0]):
+                return
+
+            if flag[i][j] == src:
+                flag[i][j] = dst
+                infect(i - 1, j, src, dst, flag)
+                infect(i, j - 1, src, dst, flag)
+                infect(i, j + 1, src, dst, flag)
+                infect(i + 1, j, src, dst, flag)
+            else:
+                return
+
         init = Node(0)
         flag = [[init for i in range(len(grid[0])+1)] for i in range(len(grid)+1)]
+        node_list = []
         mmax = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
@@ -13,30 +28,25 @@ class Solution:
                     if flag[i+1][j].val == 0 and flag[i][j+1].val == 0:
                         mmax += 1
                         n = Node(mmax)
-                        print(i, j, 'add')
+                        node_list.append(n)
                         flag[i+1][j+1] = n
                     elif (flag[i+1][j].val * flag[i][j+1].val) == 0:
                         flag[i+1][j+1] = flag[i+1][j] if (flag[i+1][j].val > flag[i][j+1].val) else flag[i][j+1]
+
                     elif flag[i+1][j].val == flag[i][j+1].val:
-                        if i == 8 and j == 2:
-                            print(flag[i+1][j].val, flag[i][j+1].val)
                         flag[i+1][j+1] = flag[i][j+1]
                     elif flag[i+1][j].val != flag[i][j+1].val:
-                        print(i, j, 'delete')
                         mmax -= 1
+                        for node in node_list:
+                            if node.val > max(flag[i+1][j].val, flag[i][j+1].val):
+                                node.val -= 1
                         if (flag[i+1][j].val < flag[i][j+1].val):
                             flag[i+1][j+1] = flag[i+1][j]
-                            flag[i][j+1].val = flag[i+1][j].val
+                            infect(i, j + 1, flag[i][j+1], flag[i+1][j+1], flag)
                         else:
-                            flag[i+1][j+1] =flag[i][j+1]
-                            flag[i+1][j].val = flag[i+1][j+1].val
-        temp = []
-        for i in range(len(flag)):
-            tmp = []
-            for j in range(len(flag[0])):
-                tmp.append(flag[i][j].val)
-            # temp.append(tmp)
-            print(tmp)
+                            flag[i+1][j+1] = flag[i][j+1]
+                            infect(i + 1, j, flag[i+1][j], flag[i+1][j+1], flag)
+
         return mmax
 
 
