b0914dd53f795b51580d524585563a2a561c543a
yuxzho <1701210384@pku.edu.cn>
2021-09-22 22:25:14 +0800

    22/09/21

--- 234.回文链表.py
+++ 234.回文链表.py
@@ -6,3 +6,39 @@ class ListNode:
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
+        if not head:
+            return True
+
+        firstHalfEnd = self.endOfHalf(head)
+        secondHalfStart = self.reverseList(firstHalfEnd.next)
+
+        firstPosition = head
+        secondPosition = secondHalfStart
+        while secondPosition:
+            if firstPosition.val != secondPosition.val:
+                return False
+            firstPosition = firstPosition.next
+            secondPosition = secondPosition.next
+
+        firstHalfEnd.next = self.reverseList(secondHalfStart)
+        return True
+
+    def endOfHalf(self, head: ListNode):
+        fast = head
+        slow = head
+        while fast.next is not None and fast.next.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def reverseList(self, head: ListNode):
+        pre = None
+        cur = head
+
+        while cur:
+            next = cur.next
+            cur.next = pre
+            pre = cur
+            cur = next
+
+        return pre
