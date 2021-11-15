d95e6e2484d290f218ce075d3a5001d9bf792b5f
yuxzho <1701210384@pku.edu.cn>
2021-11-15 21:48:00 +0800

    15/11/21

---
+++ 338.比特位计数.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countBits(self, n: int):
+        '''
+        1、如果 i 为偶数，那么f(i) = f(i/2) ,因为 i/2 本质上是i的二进制左移一位，低位补零，所以1的数量不变。
+        2、如果 i 为奇数，那么f(i) = f(i - 1) + 1， 因为如果i为奇数，那么 i - 1必定为偶数，而偶数的二进制最低位一定是0，
+        那么该偶数 +1 后最低位变为1且不会进位，所以奇数比它上一个偶数bit上多一个1，即 f(i) = f(i - 1) + 1。
+        '''
+        res = [0]
+        for i in range(1, n + 1):
+            if i % 2 == 0:
+                res.append(res[i >> 1])
+            else:
+                res.append(res[i - 1] + 1)
+
+        return res
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.countBits(5))
