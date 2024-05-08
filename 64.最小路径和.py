358b9eb707364976abdc2f0efc841b1672d8fcee
yuxzho <1701210384@pku.edu.cn>
2024-05-08 23:50:26 +0800

    08/05/24

--- 64.最小路径和.py
+++ 64.最小路径和.py
@@ -2,19 +2,20 @@ class Solution:
     def minPathSum(self, grid) -> int:
         m = len(grid)
         n = len(grid[0])
-        dp = [[0 for i in range(n)] for j in range(m)]
-        dp[-1][-1] = grid[-1][-1]
+        dp = [[0 for j in range(n)] for i in range(m)]
 
-        for i in range(m - 2, -1, -1):
-            dp[i][-1] = dp[i + 1][-1] + grid[i][-1]
-        for j in range(n - 2, -1, -1):
-            dp[-1][j] = dp[-1][j + 1] + grid[-1][j]
+        dp[0][0] = grid[0][0]
+        for i in range(1, m):
+            dp[i][0] = dp[i-1][0] + grid[i][0]
+        for j in range(1, n):
+            dp[0][j] = dp[0][j-1] + grid[0][j]
 
-        for i in range(m - 2, -1, -1):
-            for j in range(n - 2, -1, -1):
-                dp[i][j] = min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
 
-        return dp[0][0]
+        return dp[-1][-1]
+
 
 
 if __name__ == '__main__':
